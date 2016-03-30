0918d9036ecadda2f10e9ea408d66add7d055236
dataking <dataking@gmail.com>
2016-03-29 23:39:03 -0700

    Changed the colored text to use Term::ANSIColor, for cleaner look and easier readability.

--- var/smoothwall/mods/backupssh/install-backupssh.pl
+++ var/smoothwall/mods/backupssh/install-backupssh.pl
@@ -5,48 +5,50 @@ use lib "/usr/lib/smoothwall";
 use SmoothInstall qw( :standard );
 use header  qw( :standard );
 use Shell qw(rm touch);
+use Term::ANSIColor;
 
 Init("backupssh");
 
 # Start Installation
 # ClearScreen;
 
-PRINT $GREEN . "Installing $CYAN $ModDetails{'MOD_NAME'} $ModDetails{'MOD_VERSION'} $NORMAL";
+print colored("Installing ", "green");
+print colored("$ModDetails{'MOD_NAME'} $ModDetails{'MOD_VERSION'} \n", "cyan");
 
 # Insert Additional Code Here as required
-PRINT $GREEN . "Backing up original script...\n$NORMAL";
+print colored("Backing up original script...\n", "cyan");
 system("/bin/mv","/httpd/cgi-bin/backup.img","$MODDIR/backup/backup.img");
-print $YELLOW . "\t\t\t\t\tDone!\n\n$NORMAL";
+print colored("\t\t\t\t\tDone!\n\n", "yellow");
 
-PRINT $GREEN . "Creating keyfile in /root/.ssh...\n$NORMAL";
+print colored("Creating keyfile in /root/.ssh...\n", "green");
 system("/usr/bin/ssh-keygen -t rsa");
-print $YELLOW . "\t\t\t\t\tDone!\n\n$NORMAL";
+print colored("\t\t\t\t\tDone!\n\n", "yellow");
 
-PRINT $GREEN . "Applying necessary permissions on keyfile...\n$NORMAL";
+print colored("Applying necessary permissions on keyfile...\n", "green");
 system("chmod 600 /root/.ssh/id_*");
-print $YELLOW . "\t\t\t\t\tDone!\n\n$NORMAL";
+print colored("\t\t\t\t\tDone!\n\n", "yellow");
 
-PRINT $GREEN . "Applying necessary permissions on mod dir...\n$NORMAL";
+print colored("Applying necessary permissions on mod dir...\n", "green");
 system("chown -R nobody.root $MODDIR/etc");
-print $YELLOW . "\t\t\t\t\tDone!\n\n$NORMAL";
+print colored("\t\t\t\t\tDone!\n\n", "yellow");
 
-PRINT $GREEN . "Preparing for the future non-interactive SCP transfer with remote host...";
-print "\nPlease enter remote hostname or IP:\n$NORMAL";
+print colored("Preparing for the future non-interactive SCP transfer with remote host...\n", "green");
+print colored("Please enter remote hostname or IP:\n", "green");
 $host = "";
 $host = <STDIN>;
 chomp $host;
-print $GREEN . "\nPlease enter remote host SSH port:\n$NORMAL";
+print colored("\nPlease enter remote host SSH port:\n", "green");
 $port = "";
 $port = <STDIN>;
-chomp $port;
-print $GREEN . "\nPlease enter remote host user: [root]\n$NORMAL";
+chomp($port);
+print colored("\nPlease enter remote host user: [root]\n", "green");
 $user = "";
 $user = <STDIN>;
-chomp $user;
+chomp($user);
 if ($user eq '') { $user = 'root'; }
-print $GREEN . "\nAdding key to remote host's authorized_keys file...\n$NORMAL";
+print colored("\nAdding key to remote host's authorized_keys file...\n", "green");
 system("scp -P $port /root/.ssh/id_rsa.pub $user\@$host:.; ssh -p $port $user\@$host \"mkdir -p /$user/.ssh; cd /$user/.ssh; /bin/touch authorized_keys; cat /$user/id_rsa.pub >> authorized_keys\"");
-print $YELLOW . "\t\t\t\t\tDone!\n\n$NORMAL";
+print colored("\t\t\t\t\tDone!\n\n", "yellow");
 
 $cgiparams{'HOST'} = $host;
 $cgiparams{'PORT'} = $port;
@@ -57,7 +59,8 @@ $cgiparams{'KEY'} = 'id_rsa';
 # Remove Installation Symbolic Link
 # unlink("/tmp/install-$ModDetails{'MOD_NAME'}"); # No longer needed with makeself?
 
-PRINT $GREEN . "Finished Installation. $NORMAL";
+print colored("Finished Installation.\n", "yellow");
 # Restart Smoothd to install new module
 StopMod("/usr/sbin/smoothd", "Smoothd");
 StartMod("/usr/sbin/smoothd", "Smoothd");
+
